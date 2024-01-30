438d500c8b12f45325bb827144814b65209ed669
VincentCinque <146038947+VincentCinque@users.noreply.github.com>
2024-01-30 14:06:44 -0500

    Update w2_lab.php

--- w2_lab.php
+++ w2_lab.php
@@ -1,56 +1,24 @@
 <?php
-    /*
-    filename:
-    author name:
-    */
-    /* this is how you create variables in php*/
-    $a = true;
-    $aa = false;
-    $b = "good";
-    $c = 33;
-    $d = 33.5; /* this is a double*/
-    /* define is the same as a function in python "E" is defined and once called
-    Im a PHP constant other than a variable is output constant can only be assigned once*/
-    define("E", "I'm a PHP constant other than a variable");
-
-    /*concatenation combines two variables just like in python */
-    $first_name = 'Bob';
-    $last_name = 'Roberts';
-    //$name = 'Name: ' . $first_name;
-    $name = $first_name . ' ' . $last_name;
-    // adding the ' ' as seen above displays it with a space to make it look better
+    /*Below will save the value from the user input into the PHP file  */
+    $first_name = $_GET ['first_name'];
+    $last_name = $_GET ['last_name'];
+    /* GET will attach the users input directly into the link in the browser exposed*/
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Name Test</title>
+    <link rel= "stylesheet" type= "text/css"
+            href="main.css"/>   
 
 </head>   
 <body>
 
-    <!-- echo is basically the same thing as print in python -->
-    <h1>this is a first php prac</h1>
-    <!-- the echo below will display the literal string-->
-    <p>this is: <?php echo "abc"; ?> </p>
-    <!-- This echo will pull whatever is in the variable above (see php script above) -->
-    <p>what is: <?php echo $a; ?> </p> 
-    <p>what is: <?php echo $aa; ?> </p> 
-    <p>what is: <?php echo $b; ?> </p>
-    <p>what is: <?php echo $c; ?> </p>
-    <p>what is: <?php echo $d; ?> </p>
-    <!-- this will show what type of data is stored within the variable (gettype) -->
-    <p>type is: <?php echo gettype($b); ?> </p>
-    <!-- gettype here displays integer because php variable above contains a number -->
-    <p>type is: <?php echo gettype($c); ?> </p>
-    <!-- type is boolean -->
-    <p>type is: <?php echo gettype($a); ?> </p>
-    <p>type is: <?php echo gettype($d); ?> </p>
-    <p>type is: <?php echo E; ?> </p>
-    <p>type is: <?php echo gettype (E); ?> </p>
-    <p>this is concatenation: <?php echo $name ?></p>
-    
-    
+    <h2>Welcome</h2>
+    <p>First Name: <?php echo $first_name; ?></p>
+    <p>Last Name: <?php echo $last_name; ?></p>
 </body>   
 
 </html>
