8106ba9b63982e4b535db71f6f78b30da6f4998e
VincentCinque <146038947+VincentCinque@users.noreply.github.com>
2024-02-01 17:33:23 -0500

    started homework

---
+++ display_discount.php
@@ -0,0 +1,40 @@
+<?php
+/*****************************************************
+ * File Name: HW_INFO152/hw1/index.php
+ * Purpose: INFO152-HW1, Build and Enchance "Product Discount'
+ * Author: Vincent Cinque
+ * DrexelId: 14678062
+ * Complete Date: 04-Feb-2024
+ ********************************************************/
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <main>
+        <h1>This page is under construction</h1>
+
+        <label>Product Description:</label>
+        <span><?php echo ''; ?></span><br>
+
+        <label>List Price:</label>
+        <span><?php echo ''; ?></span><br>
+
+        <label>Standard Discount:</label>
+        <span><?php echo ''; ?></span><br>
+
+        <label>Discount Amount:</label>
+        <span><?php echo ''; ?></span><br>
+
+        <label>Discount Price:</label>
+        <span><?php echo ''; ?></span><br>
+    </main>
+</body>
+</html>
