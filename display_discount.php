31b04d07c3c2c9bc12de626ae3b434abcc64005a
VincentCinque <146038947+VincentCinque@users.noreply.github.com>
2024-02-04 11:23:22 -0500

    finished

--- display_discount.php
+++ display_discount.php
@@ -1,9 +1,9 @@
 <?php
 /*****************************************************
- * File Name: HW_INFO152/hw1/index.php
+ * File Name: HW_INFO152/hw1/display_discount.php
  * Purpose: INFO152-HW1, Build and Enchance "Product Discount'
  * Author: Vincent Cinque
- * DrexelId: 14678062
+ * DrexelId: vrc45
  * Complete Date: 04-Feb-2024
  ********************************************************/
 //Calculate the discount amount using below variables
@@ -12,22 +12,22 @@ $list_price = $_GET ['list_price'];
 $discount_percent = $_GET ['discount_percent'];
 $discount_amount = $list_price * $discount_percent * .01;
 $discount_price = $list_price - $discount_amount;
-
-// Formatting numbers to second decimal place and adding $ sign
+/********************************************************************* */
+// Formatting numbers to second decimal place and adding $ sign**********************
 $list_price_formated = "$" .number_format($list_price, 2);
-$discount_percent = number_format($discount_percent, 1);
+$discount_percent_formated = number_format($discount_percent, 1);
 $discount_amount_formated = "$" .number_format ($discount_amount, 2);
 $discount_price_formated = "$" .number_format($discount_price, 2);
-
-// Concatenating and html formatting
+/************************************************************************************* */
+// Concatenating and html formatting**************************************************
 $product_description = htmlspecialchars($product_description);
 $discount_percent .= '%';
-
-
-//Author Homework and other information
+/*********************************************************************************** */
+//Author Homework and other information*************
 $author = '  Vincent Cinque ';
 $current_date = date(' Y/m/d ');
 $hw = ' for Info152-HW1';
+//**************************************************** */
 ?>
 
 
@@ -43,7 +43,7 @@ $hw = ' for Info152-HW1';
         <h1>Your Discount</h1>
 
         <label>Product Description:</label>
-        <span><?php echo $product_description ; ?></span><br> 
+        <span><?php echo $product_description; ?></span><br> 
 
         <label>List Price:</label>
         <span><?php echo $list_price_formated; ?></span><br>
