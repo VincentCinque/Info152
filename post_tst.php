2d9daa003ca123b57ae90229eca2732afdd0b3b4
VincentCinque <146038947+VincentCinque@users.noreply.github.com>
2024-03-08 19:45:43 -0500

    started

---
+++ post_tst.php
@@ -0,0 +1,25 @@
+<?php
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$age = $_POST["age"];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+    <h2>Please Find User Information Below Through Post Method:</h2>
+    <p><?php echo "recieved age is $age years old";?></p>
+</body>
+
+
+</html>
+
+
+
+
