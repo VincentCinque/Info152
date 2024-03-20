180896131b1f685c63950f76c3a9a19f33902752
VincentCinque <146038947+VincentCinque@users.noreply.github.com>
2024-03-20 13:50:32 -0400

    needs debugging

--- post_tst.php
+++ post_tst.php
@@ -1,7 +1,21 @@
 <?php
-$fname = $_POST["fname"];
-$lname = $_POST["lname"];
-$age = $_POST["age"];
+
+$age =filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
+$text = filter_input(INPUT_POST, "text");
+var_dump($text);
+var_dump($age);
+$array_a = array(1,2,"a",true);
+
+for($i=0; $i < 4; $i++){
+    var_dump($array_a[$i]);
+}
+$age_2 = filter_input(INPUT_POST, "age_2");
+var_dump($age_2);
+$commute = $_POST["commute"];
+echo 'using post to recieve commute array';
+var_dump($commute);
+// $commute = filter_input(INPUT_POST, "commute");
+// var_dump($commute);
 ?>
 
 
@@ -14,7 +28,44 @@ $age = $_POST["age"];
 
 <body>
     <h2>Please Find User Information Below Through Post Method:</h2>
-    <p><?php echo "recieved age is $age years old";?></p>
+    <p>
+        <?php
+            if($age){
+                echo "recived age is $age years old";
+                echo "<br>";
+                echo htmlspecialchars($text);
+                echo "<p>" . $text . "</p>";
+            }
+
+            for($i=0; $i < 4; $i++){
+                echo "<p>" . ($array_a[$i]) . "</p>";
+            }
+
+            
+
+        ?>
+     </p>
+
+     <div>
+        <p>
+        <?php
+            echo $age_2;
+            echo "<br>";
+            echo $commute;
+
+        ?>    
+        </p>
+
+     </div>
+
+     <div>
+        <?php
+
+             for($i=0; $i < 3; $i++){
+                echo "<p>" . ($commute[$i]) . "</p>";
+            }
+        ?>
+     </div>
 </body>
 
 
